02d90c3a06a57d28168858c6704bbae113f32b47
tb_bin@163.com <tb_bin@163.com>
2018-10-11 15:52:36 +0800

    HttpMethod功能开发

--- zusmart-network-http/src/main/java/com/zusmart/network/http/HttpMethod.java
+++ zusmart-network-http/src/main/java/com/zusmart/network/http/HttpMethod.java
@@ -2,4 +2,32 @@ package com.zusmart.network.http;
 
 public enum HttpMethod {
 
+	GET("GET"), HEAD("HEAD"), POST("POST"), PUT("PUT"), PATCH("PATCH"), DELETE("DELETE"), TRACE("TRACE"), CONNECT("CONNECT");
+
+	private String value;
+
+	HttpMethod(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return this.value;
+	}
+
+	@Override
+	public String toString() {
+		return this.value;
+	}
+
+	private static final HttpMethod DEFAULT = HttpMethod.GET;
+
+	public static HttpMethod parse(String value) {
+		for (HttpMethod method : HttpMethod.values()) {
+			if (method.getValue().equals(value)) {
+				return method;
+			}
+		}
+		return DEFAULT;
+	}
+
 }
