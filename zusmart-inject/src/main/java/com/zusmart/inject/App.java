008e8b93f43e0a5068f329f13d6ac986597b80a3
tb_bin@163.com <tb_bin@163.com>
2018-10-15 13:19:35 +0800

    添加依赖注入模块

---
+++ zusmart-inject/src/main/java/com/zusmart/inject/App.java
@@ -0,0 +1,13 @@
+package com.zusmart.inject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ zusmart-inject/src/test/java/com/zusmart/inject/AppTest.java
@@ -0,0 +1,5 @@
+package com.zusmart.inject;
+
+public class AppTest {
+
+}
